d5faab5df5673b62ac70fe66377bd9aab283fa28
amun3808 <75223825+amun3808@users.noreply.github.com>
2022-05-04 17:05:14 +0100

    rm duplicate text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,3 @@
-// FREQUENTLY ASKED
-const faq = [
-  // web dev/design
-  {
-    subtitle: "Web development/Design",
-    questions: [
-      {
-        q: `What types of websites can you build?`,
-        a: `Any type, from personal to business/e-commerce websites and bespoke content management platforms.`,
-      },
-      {
-        q: `How long does it take to build a website?`,
-        a: `It depends on the size of the website, but a small one takes between 3 an 7 days. Very large websites can take months.`,
-      },
-      {
-        q: `What is the price for a website?`,
-        a: `Again, it depends on the size of the website, but for a "normal" website with 3-5 pages(example: Landing page, About, Contact, Services, etc) the price starts from 600GBP`,
-      },
-      {
-        q: `Are the prices different for e-commerce sites?`,
-        a: `Yes, the prices for e-commerce sites are higher than for normal websites and they start at 1200gbp.`,
-      },
-      {
-        q: `Are the prices different for content management platforms?`,
-        a: `If I have to write a bespoke system, yes, the prices start at 900gbp. If you want me to edit an existing system, we'll have to talk about the requirements before giving you an estimate.`,
-      },
-    ],
-  },
-
-  // SEO
-  // {
-  //   subtitle: "Search Engine Optimization",
-  //   questions: [
-  //     {
-  //       q: `What types of websites can you build?`,
-  //       a: `Any type, from personal to business/e-commerce websites and bespoke content management platforms.`,
-  //     },
-  //     {
-  //       q: `How long does it take to build a business website?`,
-  //       a: `It depends on the size of the website, but it usually takes between 3 an 7 days. Very large websites can take up to a month`,
-  //     },
-  //   ],
-  // },
-];
-const faqList = document.querySelector(".faq__list");
-for (const section of faq) {
-//   faqList.innerHTML += `<div class="faq__subtitle"><p>${section.subtitle}</p></div>`;
-  for (const question of section.questions) {
-    faqList.innerHTML += `
-    <div class="faq__item">
-        <div class="faq__question">
-            <h4>${question.q}</h4>
-            <span class="faq__question-arrow"></span>
-        </div>
-        <div class="faq__answer">
-            <p>
-                ${question.a}
-            </p>
-        </div>
-    </div>
-    `;
-  }
-}
-//CODE FOR SINGLE TOGGLE
-for (const faqItem of document.querySelectorAll(".faq__question")) {
-  faqItem.addEventListener("click", (e) => {
-    const hasClass =
-      e.currentTarget.parentNode.classList.contains("faq__show-answer");
-
-    //if it doesn't have the class when we click, it means another element has it, so remove it from the others
-    if (!hasClass) {
-      for (const el of document.querySelectorAll(".faq__show-answer")) {
-        el.classList.remove("faq__show-answer");
-      }
-    }
-    //toggle the class on parent element('.faq__item')
-    e.currentTarget.parentNode.classList.toggle("faq__show-answer");
-  });
-}
-
-//CODE FOR MULTIPLE TOGGLEs
-// for (const faqItem of document.querySelectorAll(".faq__question")) {
-//   faqItem.addEventListener("click", (e) =>
-//     e.currentTarget
-//       .parentNode.classList.toggle("faq__show-answer")
-//   );
-// }
-
 const projectList = [
   {
     section: "Test section",
@@ -188,15 +100,17 @@ for (section of projectList) {
   }
 }
 
+// SERVICES-----------------------------------
+
 const servicesList = [
   {
     img: "icons8-ui-design-64.png",
     title: "Ui Design",
-    copy: `New websites built from the ground up with your business in mind. Mobile first approach to ensure your website scales to any device, no matter what size.`,
+    copy: `New websites built from the ground up with your business in mind.`,
   },
   {
     title: "Web Development",
-    copy: `I can work on existing projects too, whether you need some new functionality adding or something fixing.`,
+    copy: `I can work on existing projects too, add new functionalities or fix those stubborn, annoying bugs.`,
     img: "icons8-web-development-64.png",
   },
   {
@@ -241,3 +155,91 @@ for(const service of servicesList){
 </div>
 `
 }
+
+// FREQUENTLY ASKED QUESTIONS -----------------------
+const faq = [
+  // web dev/design
+  {
+    subtitle: "Web development/Design",
+    questions: [
+      {
+        q: `What types of websites can you build?`,
+        a: `Any type, from personal to business/e-commerce websites and bespoke content management platforms.`,
+      },
+      {
+        q: `How long does it take to build a website?`,
+        a: `It depends on the size of the website, but a small one takes between 3 an 7 days. Very large websites can take months.`,
+      },
+      {
+        q: `What is the price for a website?`,
+        a: `Again, it depends on the size of the website, but for a "normal" website with 3-5 pages(example: Landing page, About, Contact, Services, etc) the price starts from 600GBP`,
+      },
+      {
+        q: `Are the prices different for e-commerce sites?`,
+        a: `Yes, the prices for e-commerce sites are higher than for normal websites and they start at 1200gbp.`,
+      },
+      {
+        q: `Are the prices different for content management platforms?`,
+        a: `If I have to write a bespoke system, yes, the prices start at 900gbp. If you want me to edit an existing system, we'll have to talk about the requirements before giving you an estimate.`,
+      },
+    ],
+  },
+
+  // SEO
+  // {
+  //   subtitle: "Search Engine Optimization",
+  //   questions: [
+  //     {
+  //       q: `What types of websites can you build?`,
+  //       a: `Any type, from personal to business/e-commerce websites and bespoke content management platforms.`,
+  //     },
+  //     {
+  //       q: `How long does it take to build a business website?`,
+  //       a: `It depends on the size of the website, but it usually takes between 3 an 7 days. Very large websites can take up to a month`,
+  //     },
+  //   ],
+  // },
+];
+const faqList = document.querySelector(".faq__list");
+for (const section of faq) {
+//   faqList.innerHTML += `<div class="faq__subtitle"><p>${section.subtitle}</p></div>`;
+  for (const question of section.questions) {
+    faqList.innerHTML += `
+    <div class="faq__item">
+        <div class="faq__question">
+            <h4>${question.q}</h4>
+            <span class="faq__question-arrow"></span>
+        </div>
+        <div class="faq__answer">
+            <p>
+                ${question.a}
+            </p>
+        </div>
+    </div>
+    `;
+  }
+}
+//CODE FOR SINGLE TOGGLE
+for (const faqItem of document.querySelectorAll(".faq__question")) {
+  faqItem.addEventListener("click", (e) => {
+    const hasClass =
+      e.currentTarget.parentNode.classList.contains("faq__show-answer");
+
+    //if it doesn't have the class when we click, it means another element has it, so remove it from the others
+    if (!hasClass) {
+      for (const el of document.querySelectorAll(".faq__show-answer")) {
+        el.classList.remove("faq__show-answer");
+      }
+    }
+    //toggle the class on parent element('.faq__item')
+    e.currentTarget.parentNode.classList.toggle("faq__show-answer");
+  });
+}
+
+//CODE FOR MULTIPLE TOGGLEs
+// for (const faqItem of document.querySelectorAll(".faq__question")) {
+//   faqItem.addEventListener("click", (e) =>
+//     e.currentTarget
+//       .parentNode.classList.toggle("faq__show-answer")
+//   );
+// }
